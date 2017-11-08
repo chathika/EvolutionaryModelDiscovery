64727699341b569325c943579459cadae98a0fb7
chathika <chathikagunaratne@gmail.com>
2017-11-08 09:33:53 -0500

    added comments

--- EMD_0.0.1.0/EMD_Spec/GECCOReplicationForFeatureAnalysis/MyProblem.java
+++ EMD_0.0.1.0/EMD_Spec/GECCOReplicationForFeatureAnalysis/MyProblem.java
@@ -1,3 +1,24 @@
+/**
+* Evolutionary Model Discovery
+* Implemented as part of Chathika Gunaratne's PhD dissertation work
+* @author Chathika Gunaratne <chathikagunaratne@gmail.com> , <chathika@knights.ucf.edu>
+* @date Dec 2016
+*
+*	@Copyright Notice:
+*	This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package GECCOReplicationForFeatureAnalysis;
 
 import ec.util.*;
