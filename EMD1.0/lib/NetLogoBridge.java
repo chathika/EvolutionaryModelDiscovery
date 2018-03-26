d73c569a96cda3db129a1de90fe25641fca9df47
chathika <chathikagunaratne@gmail.com>
2018-03-26 01:38:32 -0400

    Added dependencies

---
+++ EMD1.0/lib/NetLogoBridge.java
@@ -0,0 +1,125 @@
+//Originally authored by D Masad
+//Customized for EMD by Chathika Gunaratne <chathikagunaratne@gmail.com>
+import py4j.GatewayServer;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import bsearch.nlogolink.NetLogoLinkException;
+import javax.imageio.ImageIO;
+import bsearch.space.*;
+
+import org.nlogo.headless.HeadlessWorkspace; 
+
+public class NetLogoBridge {
+	
+	HeadlessWorkspace ws;
+	
+	public NetLogoBridge() {
+		ws = HeadlessWorkspace.newInstance();
+	}
+
+	/**
+	 * Load a NetLogo model file into the headless workspace.
+	 * @param path: Path to the .nlogo file to load.
+	 */
+	public void openModel(String path) {
+		System.out.println("opening" + path);
+		try {
+			ws.open(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	/**
+	 * Close the Netlogo file.
+	 * 
+	 */
+	public void closeModel(){
+		try {
+			ws.dispose();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Create a new headless instance. 
+	 * Use this after closeModel() to instantiate a
+	 * new instance of Netlogo. Great for multiple runs!
+	 */
+	public void refresh(){
+		ws = HeadlessWorkspace.newInstance();
+	}
+	
+	/**
+	 * Export a view (the visualization area) to the Java file's working directory.
+	 * @param filename: Name used to save the file. Include .png (ex: file.png)
+	 */
+	public void exportView(String filename){
+		try {
+			BufferedImage img = ws.exportView();
+		    File outputfile = new File(filename);
+		    ImageIO.write(img, "png", outputfile);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}		
+	}
+	
+	/**
+	 * Send a command to the open NetLogo model.
+	 * @param command: NetLogo command syntax.
+	 */
+	public void command(String command) {
+		try {
+			ws.command(command);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	/**
+	 * Get the value of a variable in the NetLogo model.
+	 * @param command: The value to report.
+	 * @return Floating point number
+	 */
+	public Double report(String command) {
+		double report = 0.0;
+		try {
+			report = (Double)ws.report(command);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return report;
+	}
+	
+	public SearchSpace getParamList(String path) {
+		String constraintsText = "";
+		SearchSpace ss = null;
+		try{
+		constraintsText = bsearch.nlogolink.Utils.getDefaultConstraintsText(path);
+		
+		ss = new SearchSpace(java.util.Arrays.asList(constraintsText.split("\n")));
+		for(ParameterSpec paramSpec : ss.getParamSpecs()) {
+			System.out.println(paramSpec.getClass());
+		}
+		} catch (NetLogoLinkException e)
+		{
+			e.printStackTrace();			
+		}
+		return ss;
+	}
+	
+	/**
+	 * Launch the Gateway Server.
+	 */
+	public static void main(String[] args) {
+		GatewayServer gs = new GatewayServer(new NetLogoBridge());
+		gs.start();
+		System.out.println("Server running");
+
+	}
+
+}
