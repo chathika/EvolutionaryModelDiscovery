dbfc072c391a16321bb9c1db10d67c2176a6b835
chathika <chathikagunaratne@gmail.com>
2018-04-03 16:08:49 -0400

    Added fanova code from Nguyen Dang

---
+++ EMD_0.0.1.0/fANOVA_analysis/fanova_analysis_code/run-fanova.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/home/nttd/Dropbox/UHasselt/fanova/fanova-master:/$PYTHONPATH
+python2 run_fANOVA.py
---
+++ EMD_0.0.1.0/fANOVA_analysis/fanova_analysis_code/run_fANOVA.py
@@ -0,0 +1,10 @@
+import sys
+
+from pyfanova.fanova import Fanova
+f=Fanova('./')
+f.print_all_marginals()
+
+from pyfanova.visualizer import Visualizer
+vis=Visualizer(f)
+vis.create_all_plots('./plots')
+vis.create_most_important_pairwise_marginal_plots('./plots',4)
