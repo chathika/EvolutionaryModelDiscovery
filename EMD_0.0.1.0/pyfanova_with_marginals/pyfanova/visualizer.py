35303f45049f9b91ff3aedf50cd2ad87b688ee3d
chathika <chathikagunaratne@gmail.com>
2018-06-05 00:46:03 -0400

    display_grid fix to visualizer

--- EMD_0.0.1.0/pyfanova_with_marginals/pyfanova/visualizer.py
+++ EMD_0.0.1.0/pyfanova_with_marginals/pyfanova/visualizer.py
@@ -423,7 +423,7 @@ class Visualizer(object):
             outTextFile = './plots' + param_name_1 + "_" +param_name_2 + '.png.txt'
             print('saving to ' + outTextFile)
             with open(outTextFile, 'wt') as f:
-                f.write(lsOut)        
+                f.write(split.join([str(x) for x in lsOut]))        
                 
         return plt
     
