1f0d3626d6a8b63d91ddbbe474d315c818411df9
chathika <chathikagunaratne@gmail.com>
2018-06-04 23:51:29 -0400

    display_grid fix to visualizer

--- EMD_0.0.1.0/pyfanova_with_marginals/pyfanova/visualizer.py
+++ EMD_0.0.1.0/pyfanova_with_marginals/pyfanova/visualizer.py
@@ -419,7 +419,7 @@ class Visualizer(object):
         # save output to file
         split = ' '
         if self.savingOutput:
-            lsOut = [str(param_name_1 + "_" +param_name_2), split.join([str(x) for x in display_grid]), split.join([str(x) for x in mean]), split.join([str(x) for x in std])]
+            lsOut = [param_name_1, split.join([str(x) for x in display_grid_1]),param_name_2, split.join([str(x) for x in display_grid_2]), split.join([str(x) for x in mean]), split.join([str(x) for x in std])]
             outTextFile = directory + '/' + param_name_1 + "_" +param_name_2 + '.png.txt'
             print('saving to ' + outTextFile)
             with open(outTextFile, 'wt') as f:
