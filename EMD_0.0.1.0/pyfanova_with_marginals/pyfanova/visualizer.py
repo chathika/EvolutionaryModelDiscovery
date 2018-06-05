9a0e9a12932c87075b74bb8124b9aa160a185fcd
chathika <chathikagunaratne@gmail.com>
2018-06-04 23:36:42 -0400

    indentation fix to visualizer

--- EMD_0.0.1.0/pyfanova_with_marginals/pyfanova/visualizer.py
+++ EMD_0.0.1.0/pyfanova_with_marginals/pyfanova/visualizer.py
@@ -29,15 +29,15 @@ class Visualizer(object):
     def generate_new_color(self,existing_colors,pastel_factor = 0.5):
         max_distance = None
         best_color = None
-    for i in range(0,100):
-        color = self.get_random_color(pastel_factor = pastel_factor)
-        if not existing_colors:
-            return color
-        best_distance = min([self.color_distance(color,c) for c in existing_colors])
-        if not max_distance or best_distance > max_distance:
-            max_distance = best_distance
-        best_color = color
-    return best_color
+        for i in range(0,100):
+            color = self.get_random_color(pastel_factor = pastel_factor)
+            if not existing_colors:
+                return color
+            best_distance = min([self.color_distance(color,c) for c in existing_colors])
+            if not max_distance or best_distance > max_distance:
+                max_distance = best_distance
+            best_color = color
+        return best_color
 
     def __init__(self, fanova):
         self._fanova = fanova
