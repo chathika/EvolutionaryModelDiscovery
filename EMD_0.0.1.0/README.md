772241cff07ff8ca53305d74b48de2b72a6e5488
chathika <chathikagunaratne@gmail.com>
2018-06-08 17:37:52 -0400

    Removing old version of EMD and keeping it in the fanova branch

--- EMD_0.0.1.0/README.md
+++
@@ -1,5 +0,0 @@
-To compile:
-javac -cp ..\..\ecj\ecj;"..\..\Anasazi\NetLogo 6.0.1\app\netlogo-6.0.1.jar" *.java
-
-To run: 
-execute run.bat file
--- EMD_0.0.1.0/run.bat
+++
@@ -1,8 +0,0 @@
-ECHO
-ECHO Starting Evolutionary Model Discovery. Authored by Chathika Gunaratne contact: chathika@knights.ucf.edu 
-REM Change directory to ecj\ecj
-cd ecj\ecj
-REM Run ECJ on EMD Spec application. Also provide netlogo libraries path
-java -cp .;"..\..\Anasazi\NetLogo 6.0.1\app\netlogo-6.0.1.jar";..\..\EMD_Spec\ ec.Evolve -file ..\..\EMD_Spec\EMD_ArtificialAnasazi.params
-REM Change directory back
-cd ..\..
