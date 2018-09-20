ca57bba49ec70991779f5304a02e7699132151b8
chathika <chathikagunaratne@gmail.com>
2018-09-19 21:59:55 -0400

    clean up

--- EvolutionaryModelDiscovery1.0/EMD1.0/src/EvolutionaryModelDiscovery/EMD/Util.py
+++
@@ -1,7 +0,0 @@
-import re
-def netLogoEMDLineToArray(netlogoEMDLine):
-    netlogoEMDLine = netlogoEMDLine.lower()
-    if "@emd" in netlogoEMDLine:
-        return re.sub("[\s;]","",netlogoEMDLine).split("@")
-    else:
-        print("Not and EMD annotated line!")
