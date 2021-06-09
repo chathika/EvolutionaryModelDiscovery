eefd34509e56a2668957f62a718ad8af75273520
Chathika <gunaratnecs@ornl.gov>
2021-06-09 17:43:06 -0400

    'v1.2.0'

--- src/EvolutionaryModelDiscovery/setup.py
+++ src/EvolutionaryModelDiscovery/setup.py
@@ -15,7 +15,7 @@ from setuptools  import setup
 
 setup(
     name='EvolutionaryModelDiscovery',
-    version='1.0.0',
+    version='1.2.0',
     author='Chathika Gunaratne',
     author_email='chathikagunaratne@gmail.com',
     packages=['EvolutionaryModelDiscovery'],
