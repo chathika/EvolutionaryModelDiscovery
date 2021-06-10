4695199444ee69e23d2da31ba2d5ccb8968091f3
chathika <chathikagunaratne@gmail.com>
2021-06-10 08:39:59 -0400

    v2.0.0

--- src/EvolutionaryModelDiscovery/setup.py
+++ src/EvolutionaryModelDiscovery/setup.py
@@ -15,7 +15,7 @@ from setuptools  import setup
 
 setup(
     name='EvolutionaryModelDiscovery',
-    version='1.3.0',
+    version='2.0.0rc',
     author='Chathika Gunaratne',
     author_email='chathikagunaratne@gmail.com',
     packages=['EvolutionaryModelDiscovery'],
