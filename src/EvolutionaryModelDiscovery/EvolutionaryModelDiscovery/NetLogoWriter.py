1dd24e25fe5039200f9fc526399f295794a85b3b
co1 <gunaratnecs@ornl.gov>
2022-06-06 10:11:28 -0400

    Factors file path fix, allows script to exist in a folder other than model

--- src/EvolutionaryModelDiscovery/EvolutionaryModelDiscovery/NetLogoWriter.py
+++ src/EvolutionaryModelDiscovery/EvolutionaryModelDiscovery/NetLogoWriter.py
@@ -12,11 +12,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>."""
 
+from .Util import *
 from pathlib import Path
 import uuid
 import re
-
-from .Util import *
 
 
 class NetLogoWriter:
@@ -38,26 +37,39 @@ class NetLogoWriter:
 
         # find EMD entry point
         factors_file_replace = re.compile(
-            re.escape('factors-file='), re.IGNORECASE)
+            re.escape("factors-file="), re.IGNORECASE
+        )
         return_type_replace = re.compile(
-            re.escape('return-type='), re.IGNORECASE)
-        with open(self._original_model_path, 'r') as file_reader:
+            re.escape("return-type="), re.IGNORECASE
+        )
+        with open(self._original_model_path, "r") as file_reader:
             for i, line in enumerate(file_reader):
-                if "@emd" in line.lower() and "@evolvenextline" in line.lower():
+                if (
+                    "@emd" in line.lower()
+                    and "@evolvenextline" in line.lower()
+                ):
                     self._EMD_line = i + 1
                     emd_parameters = netlogo_EMD_line_to_array(line)[3:]
                     for emd_parameter in emd_parameters:
                         if "factors-file=" in emd_parameter.lower():
                             rel_factors_file_path = Path(
-                                factors_file_replace.sub("", emd_parameter))
+                                factors_file_replace.sub("", emd_parameter)
+                            )
                             self._factors_file_path = str(
-                                rel_factors_file_path.absolute())
+                                Path.joinpath(
+                                    Path(model_path).parents[0],
+                                    rel_factors_file_path,
+                                )
+                            )
                         elif "return-type=" in emd_parameter.lower():
                             self._EMD_return_type = f'EMD{return_type_replace.sub("", emd_parameter)}'
                             self._EMD_return_type = slugify(
-                                self._EMD_return_type)
+                                self._EMD_return_type
+                            )
             file_reader.close()
-        assert (self._EMD_line > 0), "No @EMD @EvolveNextLine annotation detected!"
+        assert (
+            self._EMD_line > 0
+        ), "No @EMD @EvolveNextLine annotation detected!"
 
     def get_factors_file_path(self) -> str:
         """
@@ -77,27 +89,29 @@ class NetLogoWriter:
 
     def inject_new_rule(self, new_rule: str) -> str:
         """
-        Injects new rule into the line following the @EvolveNextLine annotation and saves it as a 
-        .EMD.nlogo model file. 
+        Injects new rule into the line following the @EvolveNextLine annotation and saves it as a
+        .EMD.nlogo model file.
 
         :param new_rule: new rule to be injected into the model.
         :returns: path to modified model file.
         """
 
-        with open(self._original_model_path, 'r') as file:
+        with open(self._original_model_path, "r") as file:
             data = file.readlines()
             file.close()
         dir = Path(self._original_model_path).parent.absolute()
         model_name = Path(self._original_model_path).stem
         uniq = slugify(uuid.uuid4().hex)
-        rule_injected_model_path = Path(dir, f'{model_name}_{uniq}.EMD.nlogo')
-        rule_injected_model_path.parent.absolute().mkdir(parents=True, exist_ok=True)
+        rule_injected_model_path = Path(dir, f"{model_name}_{uniq}.EMD.nlogo")
+        rule_injected_model_path.parent.absolute().mkdir(
+            parents=True, exist_ok=True
+        )
         if not (Path.is_file(rule_injected_model_path)):
             # Model already injected with this rule. Using cached version.
             if self._EMD_line >= 0:
                 data[self._EMD_line] = new_rule
                 # And write everything to new model file.
-                with open(rule_injected_model_path, 'w') as file:
+                with open(rule_injected_model_path, "w") as file:
                     file.writelines(data)
                     file.flush()
                     file.close()
